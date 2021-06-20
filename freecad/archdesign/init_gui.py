2109f00afb94d4d43354b937202639319795c541
carlopav <carlopav@gmail.com>
2021-06-20 22:54:09 +0200

    Raw import of Arch containers
    Raw import of Project, Site, Building, BuildingPart.

--- freecad/archdesign/init_gui.py
+++ freecad/archdesign/init_gui.py
@@ -12,13 +12,18 @@ class ArchDesign(Gui.Workbench):
     MenuText = "ArchDesign workbench"
     ToolTip = "a simple template workbench"
     Icon = os.path.join(ICONPATH, "ArchDesign_Workbench.svg")
-    toolbox = ['MakeWall', 'JoinWalls', 'ExtendWall',
+    toolbox_objects = ['MakeWall', 'JoinWalls', 'ExtendWall',
                'MakeOpeningElement','MakeDoor', 'MakeWindow',
                'MakeView'
               ]
+    toolbox_containers = ['Project', 'Site', 'Storey',
+               'Space'
+              ]
+
 
     def GetClassName(self):
         return "Gui::PythonWorkbench"
+
 
     def Initialize(self):
         """
@@ -31,12 +36,17 @@ class ArchDesign(Gui.Workbench):
         from freecad.archdesign.commands.openings import MakeWindow
         from freecad.archdesign.commands.joinwalls import JoinWalls
         from freecad.archdesign.commands.joinwalls import ExtendWall
+
+        from freecad.archdesign.commands.project import _CommandProject
+
         from freecad.archdesign.commands.view import MakeView
-        
+
         App.Console.PrintMessage("switching to archdesign\n")
 
-        self.appendToolbar("Tools", self.toolbox)
-        self.appendMenu("Tools", self.toolbox)
+        self.appendToolbar("Objects", self.toolbox_objects)
+        self.appendToolbar("Containers", self.toolbox_containers)
+        self.appendMenu("Objects", self.toolbox_objects)
+        self.appendMenu("Containers", self.toolbox_containers)
 
         Gui.addCommand('MakeWall', MakeWall())
         Gui.addCommand('JoinWalls', JoinWalls())
@@ -47,17 +57,22 @@ class ArchDesign(Gui.Workbench):
 
         Gui.addCommand('MakeView', MakeView())
 
+        Gui.addCommand('Project', _CommandProject())
+
+
     def Activated(self):
         '''
         code which should be computed when a user switch to this workbench
         '''
         pass
 
+
     def Deactivated(self):
         '''
         code which should be computed when this workbench is deactivated
         '''
         pass
+
 
 
 Gui.addWorkbench(ArchDesign())
