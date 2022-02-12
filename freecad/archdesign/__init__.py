2b24d15029b8eb87c28855c7c205cfbb422a706a
carlopav <carlopav@gmail.com>
2022-02-12 16:38:08 +0100

    Temporary comment import export ifc modules for startup speed

--- freecad/archdesign/__init__.py
+++ freecad/archdesign/__init__.py
@@ -1,4 +1,11 @@
 import os
 from .version import __version__
+import FreeCAD as App
+
+#import freecad.archdesign.import_ifc
+#import freecad.archdesign.export_ifc
 
 ICONPATH = os.path.join(os.path.dirname(__file__), "resources", "icons")
+
+#App.addImportType("ArchDesign Industry Foundation Classes (*.ifc)","freecad.archdesign.import_ifc")
+#App.addExportType("ArchDesign Industry Foundation Classes (*.ifc)","freecad.archdesign.export_ifc")
